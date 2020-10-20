3be70cf0f97c9ae79649089910a8ddec3f9ea788
GregoryVins <gregoryvins@gmail.com>
2020-10-20 13:19:59 +0300

    Lesson 1 (WSGI)     - two view (index.html, contacts.html)     - README.md (edited)     - template view.py with two functions     - main.py (2FC)     - Application

---
+++ lesson 1/source/Application.py
@@ -0,0 +1,28 @@
+class Application:
+    """
+    Создаем класс, чтобы пробросить в него данные без нарушения PEP3333.
+    """
+    def __init__(self, routes: dict, f_controllers: list):
+        """
+        Создаём атрибуты класса, где:
+        :param routes: словарь, dict - urlpatterns.
+        :param f_controllers: список, массив, list - front controllers.
+        """
+        self.routes = routes
+        self.f_controllers = f_controllers
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO']
+        if path in self.routes:
+            view = self.routes[path]
+
+            request = {}
+            for front_controller in self.f_controllers:
+                front_controller(request)
+
+            code, body = view(request)
+            start_response(code, [('Context-Type', 'text/html')])
+            return body
+        start_response = ('404 NOT FOUND', [('Context-Type', 'text/html')])
+        return [b'<h1>404 NOT FOUND<h1>']
+
