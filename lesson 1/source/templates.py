cfbd753e10df1cfccf15aaf2ebcc8f5702208d68
GregoryVins <gregoryvins@gmail.com>
2020-10-20 17:09:46 +0300

    Lesson 1 (WSGI)     - three view (index, contacts, error)     - README.md (edited)     - template view.py with two functions     - main.py (2FC)     - Application (source)     - templates (source)     - 404 error for unknown address

--- lesson 1/source/templates.py
+++ lesson 1/source/templates.py
@@ -2,14 +2,13 @@ import os
 from jinja2 import Template
 
 
-def render(template_name, folder='templates', **kwargs):
+def render(template_name, **kwargs):
     """
     Минимальная функциональность шаблонизатора.
-    :param folder: Имя папки, где находится шаблон.
     :param template_name: Имя шаблона.
     :param kwargs: Параметры для передачи.
     """
-    path = os.path.join(folder, template_name)
+    path = os.path.join('templates/', template_name)
     with open(path, encoding='UTF-8')as file:
         template = Template(file.read())
     return template.render(**kwargs)
